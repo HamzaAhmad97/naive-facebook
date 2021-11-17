a6e720acb791477e12bb69514a8e5e5ba0555106
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-17 18:08:32 +0200

    feat: added to custom user model, and made migrations

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    age = models.IntegerField(max_length=3, blank=True, null=True)
 
     def __str__(self):
         return self.email
