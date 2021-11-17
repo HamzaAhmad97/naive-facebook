ed78c56bad7c3d47882e77b5c1c9ceaf73799937
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-17 18:32:55 +0200

    feat: added Post view

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.deletion import CASCADE
+from django.urls import reverse
+class Post(models.Model):
+    content = models.TextField()
+    added_by = models.ForeignKey(get_user_model(),
+        null=True, blank=True, on_delete=CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.content[:15]
+
+    def get_absolute_url(self):
+        return reverse('posts_list')
